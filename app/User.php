f453dc95f75dcb9a75a4cc0430b2db2975daa01c
Pratik Jethaliya <jethaliyapratik@gmail.com>
2018-10-22 22:53:17 -0400

    admin master page setup

--- app/User.php
+++ app/User.php
@@ -27,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**Get role for the user
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +19,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin',function (){
+    return view('admin.index');
+});
+
+Route::resource('admin/users','AdminUsersController');
